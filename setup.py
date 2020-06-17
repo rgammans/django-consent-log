627a76ccf8bb65edcb91f68bd24f495ea0b55bd4
Roger Gammans <rgammans@gammascience.co.uk>
2020-06-17 11:33:15 +0100

    Add long_description and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
+import pathlib
+
+projectdir = pathlib.Path(__file__).parent
+readme = (projectdir / "README.rst").read_text()
+
 
 setup(name='consent_log',
       version='0.1',
       description='Keeps a record of consents. Required by EU law',
+      long_description=readme,
+      long_description_content_type="text/x-rst",
       url='http://github.com/rgammans/django-consent-log',
       author='Gamma Science',
       author_email='info@gammascience.co.uk',
@@ -12,6 +19,19 @@ setup(name='consent_log',
                 'consent_log.management.commands'
       ],
       requires = [ 'django' ],
+      classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
+      ],
       zip_safe=False,
       install_requires=[
         "Django>=2.0.0",
